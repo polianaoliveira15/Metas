cc0a304be158284677e5a24f209ac4d1f736930d
polianaoliveira15 <105360435+polianaoliveira15@users.noreply.github.com>
2024-04-11 08:34:39 -0300

    0k

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-07-23T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo3 = new Date("2025-01-15T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-31T00:00:00");
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
